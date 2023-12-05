df22b7e716fcbe7677d32c0a41cf2210b98a5335
Samuli Seppänen <samuli.seppanen@puppeteers.net>
2023-12-05 12:56:44 +0200

    cppcms: package into Debian format

--- cppcms/build.sh
+++ cppcms/build.sh
@@ -3,9 +3,13 @@
 # Build cppcms
 cd $BASEDIR
 
-git clone https://github.com/artyom-beilis/cppcms.git
+git clone --branch $REF https://github.com/artyom-beilis/cppcms.git
 cd cppcms
 
 cmake . -DCMAKE_INSTALL_PREFIX=$OUTPUT/cppcms/usr/local
 make
 make install
+
+cd $OUTPUT/cppcms
+
+fpm -t deb --force --name cppcpms --description "${FPM_DESCRIPTION}" --license "${FPM_LICENSE}" --vendor "${FPM_VENDOR}" --maintainer "${FPM_MAINTAINER}" --url "${FPM_URL}" --version 2.0.0 --iteration 1 -s dir .
