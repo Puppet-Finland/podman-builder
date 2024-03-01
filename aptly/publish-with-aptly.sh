7255554abb1a9be19bbabc37d5d568e9a057052b
Samuli Seppänen <samuli.seppanen@gmail.com>
2024-03-01 15:08:48 +0200

    aptly: drop existing repos before rebuilding them
    This allows running publish.sh multiple times without errors
    
    Signed-off-by: Samuli Seppänen <samuli.seppanen@gmail.com>

--- aptly/publish-with-aptly.sh
+++ aptly/publish-with-aptly.sh
@@ -3,13 +3,25 @@
 # Proof of concept script for publishing packages generated with podman-builds
 # using aptly
 
-# Publish Ubuntu 22.04 packages
+# Unpublish existing repositories
+aptly publish drop mantic filesystem:jammy:jammy
+aptly publish drop mantic filesystem:mantic:mantic
+
+# Drop existing snapshots
+aptly snapshot drop default_snapshot_jammy
+aptly snapshot drop default_snapshot_mantic
+
+# Drop repositories
+aptly repo drop default_repo_jammy
+aptly repo drop default_repo_mantic
+
+# (Re)publish Ubuntu 22.04 packages
 aptly repo create default_repo_jammy
 aptly repo add default_repo_jammy /home/ubuntu/output/ubuntu-22.04/
 aptly snapshot create default_snapshot_jammy from repo default_repo_jammy
 aptly publish snapshot -distribution="jammy" default_snapshot_jammy filesystem:jammy:jammy
 
-# Publish Ubuntu 23.10 packages
+# (Re)publish Ubuntu 23.10 packages
 aptly repo create default_repo_mantic
 aptly repo add default_repo_mantic /home/ubuntu/output/ubuntu-23.10/
 aptly snapshot create default_snapshot_mantic from repo default_repo_mantic
