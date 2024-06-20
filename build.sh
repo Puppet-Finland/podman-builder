fe459f962c14cc61b29b5662e75a663343cd2122
Samuli Seppänen <samuli.seppanen@puppeteers.net>
2024-06-20 20:07:48 +0300

    Support out of the tree builds
    Now project directories can be placed anywhere on the filesystem, not
    just the root of the podman-builders directory.
    
    Signed-off-by: Samuli Seppänen <samuli.seppanen@puppeteers.net>

--- build.sh
+++ build.sh
@@ -11,22 +11,23 @@ if [ "$1" = "" ] || [ "$2" = "" ]; then
     usage
 fi
 
-if ! [ -d "./${1}" ]; then
+PROJECT_DIR=$1
+if ! [ -d "${1}" ]; then
     echo "ERROR: build directory ${1} not found!"
     exit 1
 fi
 
-PROJECT=$(echo $1|tr -d "/")
+PROJECT=$(basename $1)
 DISTRO=$2
 
 # Check that distro is supported
-test -f "${PROJECT}/Containerfile.${DISTRO}"
+test -f "${PROJECT_DIR}/Containerfile.${DISTRO}"
 
 if [ $? -ne 0 ]; then
     echo "ERROR: unsupported distro ${DISTRO} for project ${PROJECT}!"
     echo
     echo "Supported distros are:"
-    ls $PROJECT/Containerfile.*|cut -d "/" -f 2|cut -d "." -f 2-
+    ls $PROJECT_DIR/Containerfile.*|cut -d "/" -f 2|cut -d "." -f 2-
     echo
     usage
 fi
@@ -34,12 +35,12 @@ fi
 IMAGE="${PROJECT}-${DISTRO}-build"
 CONTAINER="${IMAGE}-instance"
 
-if [ -r "${PROJECT}/build-customizations.env" ]; then
-    CUSTOM_ENV_FILE_PARAM="--env-file=${PROJECT}/build-customizations.env"
+if [ -r "${PROJECT_DIR}/build-customizations.env" ]; then
+    CUSTOM_ENV_FILE_PARAM="--env-file=${PROJECT_DIR}/build-customizations.env"
 else
     CUSTOM_ENV_FILE_PARAM=""
 fi
 
-podman build $PROJECT/ -f "Containerfile.${DISTRO}" -t $IMAGE
+podman build $PROJECT_DIR/ -f "Containerfile.${DISTRO}" -t $IMAGE
 podman container rm $CONTAINER
-podman run -it --name $CONTAINER --env-file=${PROJECT}/build-defaults.env $CUSTOM_ENV_FILE_PARAM -v podman-builds:/output "localhost/$IMAGE"
+podman run -it --name $CONTAINER --env-file=${PROJECT_DIR}/build-defaults.env $CUSTOM_ENV_FILE_PARAM -v podman-builds:/output "localhost/$IMAGE"
