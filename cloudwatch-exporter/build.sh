ffd4d25cdef7365d7ef5a95a4c1006e71316625c
Samuli Seppänen <samuli.seppanen@puppeteers.net>
2024-09-18 12:03:00 +0300

    cloudwatch-exporter: don't add old packages into new ones
    Also fix wrong dependency name on Rocky Linux 9

--- cloudwatch-exporter/build.sh
+++ cloudwatch-exporter/build.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 #
+FPM_TARGET_DIR="${OUTPUT}/cloudwatch_exporter/target"
+BIN_DIR="${FPM_TARGET_DIR}/opt/cloudwatch_exporter"
+UNIT_DIR="${FPM_TARGET_DIR}/etc/systemd/system"
+
+# Clean up traces of the old build
+rm -rf "${FPM_TARGET_DIR}"
+mkdir -p "${BIN_DIR}"
+mkdir -p "${UNIT_DIR}"
+
 # Build cloudwatch-exporter
 cd $BASEDIR
 
@@ -8,14 +17,9 @@ cd cloudwatch_exporter
 
 mvn package
 
-FPM_TARGET_DIR="${OUTPUT}/cloudwatch_exporter"
-BIN_DIR="${FPM_TARGET_DIR}/opt/cloudwatch_exporter"
-UNIT_DIR="${FPM_TARGET_DIR}/etc/systemd/system"
-
-mkdir -p "${BIN_DIR}"
-mkdir -p "${UNIT_DIR}"
-
 cp "${BASEDIR}/cloudwatch-exporter.service" "${UNIT_DIR}/"
 cp "${BASEDIR}/cloudwatch_exporter/target/cloudwatch_exporter-${FPM_VERSION}-jar-with-dependencies.jar" "${BIN_DIR}/cloudwatch_exporter-${FPM_VERSION}.jar"
 
 "${BASEDIR}/package.sh"
+
+cp -v $FPM_TARGET_DIR/*.deb $FPM_TARGET_DIR/*.rpm $OUTPUT/cloudwatch_exporter 2> /dev/null
--- cloudwatch-exporter/package_rocky.sh
+++ cloudwatch-exporter/package_rocky.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-FPM_TARGET_DIR="${OUTPUT}/cloudwatch_exporter"
+FPM_TARGET_DIR="${OUTPUT}/cloudwatch_exporter/target"
 
 cd $FPM_TARGET_DIR
 
-fpm -t rpm --force --name "${FPM_NAME}" --description "${FPM_DESCRIPTION}" --license "${FPM_LICENSE}" --vendor "${FPM_VENDOR}" --maintainer "${FPM_MAINTAINER}" --url "${FPM_URL}" --version "${FPM_VERSION}" --iteration "${FPM_ITERATION}" --after-install "${BASEDIR}/after-install.sh" -d "default-jre" -s dir .
+fpm -t rpm --force --name "${FPM_NAME}" --description "${FPM_DESCRIPTION}" --license "${FPM_LICENSE}" --vendor "${FPM_VENDOR}" --maintainer "${FPM_MAINTAINER}" --url "${FPM_URL}" --version "${FPM_VERSION}" --iteration "${FPM_ITERATION}" --after-install "${BASEDIR}/after-install.sh" -d "java-11-openjdk" -s dir .
